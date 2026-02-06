deb1f70796e318c9b2b038fcf0800f11bfc483e5
Christian Espino <c.espinoelvira@gmail.com>
2026-02-06 14:59:36 -0600

    Cors configurado, clases añadidas

---
+++ api-2-persistencia/src/main/java/com/christian/persistencia/configuration/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.christian.persistencia.configuration;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+    CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Content-Type"));
+        config.setAllowCredentials(false);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+}
